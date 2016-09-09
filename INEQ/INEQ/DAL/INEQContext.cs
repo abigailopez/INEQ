6c4c902572ffdeab3036caa6802e89009e1ab1db
Brenda <Brenda@ELIZABETH>
2016-09-08 22:23:30 -0500

    Se agregó el contexto en la carpeta DAL.

---
+++ INEQ/INEQ/DAL/INEQContext.cs
@@ -0,0 +1,21 @@
+﻿using INEQ.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace INEQ.DAL
+{
+    public class INEQContext : DbContext
+    {
+        public INEQContext() : base("INEQ Context")
+            {
+            }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<EquipmentType> EquipmentTypes { get; set; }
+        public DbSet<ComponentType> ComponentTypes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
