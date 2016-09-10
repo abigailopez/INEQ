dad1e646e8dc227a57cb6375ee4069626a51a8f4
Brenda <Brenda@ELIZABETH>
2016-09-10 03:57:49 -0500

    Reemplazo de Controladores y Vistas por unas vacíos y creación de métodos CRUD para "ComponentTypeController".

--- INEQ/INEQ/Controllers/ComponentTypesController.cs
+++ INEQ/INEQ/Controllers/ComponentTypesController.cs
@@ -1,128 +1,59 @@
-﻿using System;
+﻿using INEQ.DAL;
+using INEQ.Models;
+using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using INEQ.DAL;
-using INEQ.Models;
+using System.Data.Entity;
 
 namespace INEQ.Controllers
 {
     public class ComponentTypesController : Controller
     {
-        private INEQContext db = new INEQContext();
+        INEQContext dc = new INEQContext();
 
-        // GET: ComponentTypes
-        public ActionResult Index()
+      //LIST
+        public ActionResult List()
         {
-            return View(db.ComponentTypes.ToList());
+            return View(dc.ComponentTypes.ToList());
         }
 
-        // GET: ComponentTypes/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Details(int id = 0)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ComponentType componentType = db.ComponentTypes.Find(id);
-            if (componentType == null)
-            {
-                return HttpNotFound();
-            }
-            return View(componentType);
+            return View(dc.ComponentTypes.Find(id));
         }
 
-        // GET: ComponentTypes/Create
+        //CREATE
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: ComponentTypes/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Description,Active")] ComponentType componentType)
+        [HttpPost,ValidateAntiForgeryToken]
+        public ActionResult Create(ComponentType ctp)
         {
-            if (ModelState.IsValid)
+            using (dc)
             {
-                db.ComponentTypes.Add(componentType);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                dc.ComponentTypes.Add(ctp);
+                dc.SaveChanges();
             }
+            return RedirectToAction("List");
 
-            return View(componentType);
         }
 
-        // GET: ComponentTypes/Edit/5
-        public ActionResult Edit(int? id)
+        //UPDATE
+        public ActionResult Edit(int id = 0)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ComponentType componentType = db.ComponentTypes.Find(id);
-            if (componentType == null)
-            {
-                return HttpNotFound();
-            }
-            return View(componentType);
+            return View(dc.ComponentTypes.Find(id));
         }
 
-        // POST: ComponentTypes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Description,Active")] ComponentType componentType)
+        [HttpPost,ValidateAntiForgeryToken]
+        public ActionResult Edit(ComponentType ctp)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(componentType).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(componentType);
-        }
-
-        // GET: ComponentTypes/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ComponentType componentType = db.ComponentTypes.Find(id);
-            if (componentType == null)
-            {
-                return HttpNotFound();
-            }
-            return View(componentType);
-        }
-
-        // POST: ComponentTypes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            ComponentType componentType = db.ComponentTypes.Find(id);
-            db.ComponentTypes.Remove(componentType);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            dc.Entry(ctp).State = EntityState.Modified;
+            dc.SaveChanges();
+            return RedirectToAction("List");
         }
     }
 }
