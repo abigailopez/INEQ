c8ac41eb11833e4d736c691da7a8efda222e4769
ErickaHernandez <ericka_bfc@hotmail.com>
2016-09-10 16:13:06 -0500

    tabla Status

---
+++ INEQ/INEQ/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.Models
+{
+    public class Status
+    {
+        public int ID { get; set; }
+        public String Description { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
