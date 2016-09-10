c747d6f55db5e562813c371106dd1030c02b76d0
saaptic <saap_tic@hotmail.com>
2016-09-10 14:13:05 -0500

    Warehouse

---
+++ INEQ/INEQ/Models/Warehouse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.Models
+{
+    public class Warehouse
+    {
+
+        public int Id { get; set;  }
+        public String Description { get; set; }
+
+        public float  UsefulLife { get; set; }
+        public float GuaranteeDuration { get; set; }
+       
+        public Boolean active { get; set; }
+
+
+        public virtual ICollection<Equipment> Equipments { get; set; }
+}
+}
