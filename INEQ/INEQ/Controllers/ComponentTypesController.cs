ec800e70f846d427b38b0e5fc5da8e27105184ec
Brenda <Brenda@ELIZABETH>
2016-09-10 00:07:10 -0500

    Agregado "ComponentTypesController".

---
+++ INEQ/INEQ/Controllers/ComponentTypesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using INEQ.DAL;
+using INEQ.Models;
+
+namespace INEQ.Controllers
+{
+    public class ComponentTypesController : Controller
+    {
+        private INEQContext db = new INEQContext();
+
+        // GET: ComponentTypes
+        public ActionResult Index()
+        {
+            return View(db.ComponentTypes.ToList());
+        }
+
+        // GET: ComponentTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            if (componentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ComponentTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Description,Active")] ComponentType componentType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ComponentTypes.Add(componentType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            if (componentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Description,Active")] ComponentType componentType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(componentType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            if (componentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            db.ComponentTypes.Remove(componentType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
