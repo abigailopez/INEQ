ca0f95dc6d5931512a0870e9936ea36d1ff73e59
ErickaHernandez <ericka_bfc@hotmail.com>
2016-09-11 12:15:54 -0500

    camcios de INEQI E INEQC

---
+++ INEQ/INEQ/DAL/INEQC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.DAL
+{
+    public class INEQC: DbC
+    {
+        public INEQC() : base("INEQC")
+            {
+            //AQUÍ SE INSERTA LA CADENA DE CONEXIÓN
+        }
+
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+    }
+}
---
+++ INEQ/INEQ/DAL/INEQI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.DAL
+{
+    
+        public class INEQI : System.Data.Entity.DropCreateDatabaseIfModelChanges<INEQC>
+    {
+        protected override void Seed(INEQC c)
+        {
+            var statuses = new List<Status>
+                {
+                     new EquipmentType { Description = " Asignado ", UsefulLife = 3, GuaranteeDuration = 1, Active = true },
+                new EquipmentType { Description = " Sin asignar ", UsefulLife = 3, GuaranteeDuration = 1, Active = true },
+                new EquipmentType { Description = " Pre asignado ", UsefulLife = 3, GuaranteeDuration = 1, Active = true },
+            };
+
+            statuses.ForEach(s => c.Statuses.Add(s));
+            c.SaveChanges();
+
+        }
+    }
+}
+
+
