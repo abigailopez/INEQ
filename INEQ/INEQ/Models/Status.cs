f9110f2601205396ca1ffdddfa9b0ac29e65c462
ErickaHernandez <ericka_bfc@hotmail.com>
2016-09-11 12:59:52 -0500

    Se creo la tabla de Status

---
+++ INEQ/INEQ/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.Models
+{
+    public class Status
+    {
+        public String Description { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
